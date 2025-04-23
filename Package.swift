72f7d53af96ecafd3b5d7b9b95106b054e33a224
buza <kylebuza@gmail.com>
2025-04-23 14:11:21 -0500

    Move iOS version support to v14

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 let package = Package(
     name: "CognitoAuthKitiOS",
     platforms: [
-        .iOS(.v15)
+        .iOS(.v14)
     ],
     products: [
         .library(
